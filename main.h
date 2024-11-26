df705d467de30a00444d879418cbdff4108444bf
totomus83 <tom.dibellonio@gmail.com>
2024-11-26 19:27:23 +0100

    updated main with print decimal function

--- main.h
+++ main.h
@@ -13,5 +13,6 @@ int _printf(const char *format, ...);
 void print_char(va_list printfall);
 void print_string(va_list printfall);
 void print_percent();
+void print_decimal(va_list printfall);
 
 #endif
