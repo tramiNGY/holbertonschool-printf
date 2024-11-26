e6692a9871d84558edd3fc0a290fef40299c1d9f
totomus83 <tom.dibellonio@gmail.com>
2024-11-26 16:43:02 +0100

    updated the header, now contains the struct & the function we added earlier in the print_function.c, including the print character, print string and print percent functions

--- main.h
+++ main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct type
+{
+	char *specifier;
+	void (*print_type)(va_list printfall);
+} Datatype;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+void print_char(va_list, printfall);
+void print_str(va_list, printfall);
+void print_prct (va_list, printfall);
 
 #endif
