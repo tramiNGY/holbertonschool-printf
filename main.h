830e5e6a529558674cf3a2f63fb34a0cd44f17bc
tramiNGY <ngytrami@gmail.com>
2024-11-26 17:16:49 +0100

    same as for print_functions.c in all print_type deleted , after va_list

--- main.h
+++ main.h
@@ -9,8 +9,8 @@ typedef struct type
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void print_char(va_list, printfall);
-void print_string(va_list, printfall);
-void print_percent(va_list, printfall);
+void print_char(va_list printfall);
+void print_string(va_list printfall);
+void print_percent(va_list printfall);
 
 #endif
