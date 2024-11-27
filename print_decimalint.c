c331661a9b95df84a4f5bc2201178b994ecbd8b9
tramiNGY <ngytrami@gmail.com>
2024-11-27 01:13:51 +0100

    Added comments with betty style but description to be updated after function block update

--- print_decimalint.c
+++ print_decimalint.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * print_decimal - prints decimal integer (base 10) from printfall
+ * @printfall: ba_list containing all the variadic arguments
+ * to be printed
+ * Remark1: temp copies current int argument value
+ * before va_arg points to next argument
+ * Return: return number of digits printed
+ * DESCRIPTION TO BE UPDATED AFTER FUNCTION CORRECTION
+*/
+
 int print_decimal(va_list printfall)
 {
-       	int temp;
+	int temp;
 
 	temp = va_arg(printfall, int);
 	if (temp < 0)
