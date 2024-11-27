77cd8acac38be6ce24ce03eda81017a23d894b80
tramiNGY <ngytrami@gmail.com>
2024-11-27 01:08:54 +0100

    Added comments with betty style

--- print_functions.c
+++ print_functions.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * print_char - prints char argument from printfall
+ * @printfall: va_list containing all the variadic arguments
+ * to be printed
+ * Remark1: printfall va_list already defined and initialized
+ * in main function _printf
+ * Remark2: temp copies current char argument value
+ * before pointing to next argument
+ * Remark3: when passed to va_arg, char type becomes int type
+ * Return: returns 1 because 1 char is printed so
+ * total number of char printed by _printf increases by 1 (count += 1)
+*/
+
 int print_char(va_list printfall)
 {
 	char temp;
@@ -12,11 +25,22 @@ int print_char(va_list printfall)
 	return (1);
 }
 
+/**
+ * print_string - prints char * (string) argument from printfall
+ * @printfall: va_list containing all the variadic arguments
+ * to be printed
+ * Remark1: temp copies current string argument value
+ * before va_arg points to next argument
+ * Return: returns i because each time 1 char in string is printed
+ * i += 1, at end of string, i is total number of char in string
+ * so add i to count
+*/
+
 int print_string(va_list printfall)
 {
 	int i;
 	char *temp;
-	
+
 	temp = va_arg(printfall, char*);
 	i = 0;
 	while (temp[i] != '\0')
@@ -26,6 +50,14 @@ int print_string(va_list printfall)
 	}
 	return (i);
 }
+
+/**
+ * print_percent - prints char '%'
+ * @printfall: no argument associated with specifier %%
+ * so printfall parameter is ignored with line (void) printfall
+ * Return: returns (1) because '%' is 1 char printed
+ * so count increases by 1
+*/
 
 int print_percent(va_list printfall)
 {
