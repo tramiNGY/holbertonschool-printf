a65ab27060f85761c108b303371b9d4c0924a116
tramiNGY <ngytrami@gmail.com>
2024-11-27 22:32:22 +0100

    Corrected betty style and added variable intsign storing 1 if int positive abd -1 if int negative in which case len increases by 1 because printed minus sign

--- print_decimalint.c
+++ print_decimalint.c
@@ -9,33 +9,33 @@
  * before va_arg points to next argument
  * Return: return number of digits printed
  * DESCRIPTION TO BE UPDATED AFTER FUNCTION CORRECTION
- * test for git pull to be removed
  */
 
 int print_decimalint(va_list printfall)
 {
-	int temp, temptemp, templen, div, len, r;
+	int temp, temptemp, templen, div, len, r, intsign;
+
+	intsign = 1;
 
 	temp = va_arg(printfall, int);
 	if (temp == 0)
 	{
 		_putchar('0');
-		return(1);
+		return (1);
 	}
 	if (temp < 0)
 	{
 		_putchar('-');
 		temp *= -1;
+		intsign = -1;
 	}
-	temptemp = temp;
-	len = 0;
+	temptemp = temp, len = 0;
 	while (temptemp != 0)
 	{
 		temptemp /= 10;
 		len++;
 	}
-	templen = len;
-	div = 1;
+	templen = len, div = 1;
 	while (templen != 1)
 	{
 		div *= 10;
@@ -43,12 +43,13 @@ int print_decimalint(va_list printfall)
 	}
 	while (div != 0)
 	{
-		r = temp / div;
-		div /= 10;
+		r = temp / div, div /= 10;
 		if (r < 10)
 			_putchar((r) + '0');
 		else
 			_putchar((r % 10) + '0');
 	}
+	if (intsign == -1)
+		len++;
 	return (len);
 }
