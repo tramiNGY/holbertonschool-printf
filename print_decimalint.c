2eec985c9fa06ba1e4e301530b9ad46c06a5cff0
totomus83 <tom.dibellonio@gmail.com>
2024-11-28 14:45:08 +0100

    fixed the comment and changed the name of variable 'n' to number to be more explicit

--- print_decimalint.c
+++ print_decimalint.c
@@ -1,49 +1,52 @@
 #include "main.h"
 #include <stdarg.h>
 #include <limits.h>
+
 /**
  * print_decimalint - prints decimal integer (base 10) from printfall
  * @printfall: ba_list containing all the variadic arguments
  * to be printed
  * Remark1: temp copies current int argument value
  * before va_arg points to next argument
+ * Remark2: value of temp transfered to n to handle all scenarios
  * Return: return number of digits printed
- * DESCRIPTION TO BE UPDATED AFTER FUNCTION CORRECTION
+ * if number is negative add +1 to count for the '-' character
  */
+
 int print_decimalint(va_list printfall)
 {
 	int temp, div, len;
-	unsigned int min;
+	unsigned int number;
 
 	div = 1, len = 0;
 	temp = va_arg(printfall, int);
 	if (temp == INT_MIN)
 	{
 		len += _putchar('-');
-		min = (unsigned int)(-(temp + 1)) + 1;
+		number = (unsigned int)(-(temp + 1)) + 1;
 	}
 	else if (temp < 0)
 	{
 		_putchar('-');
-		min = -temp;
+		number = -temp;
 		len++;
 	}
 	else
 	{
-		min = temp;
+		number = temp;
 	}
-	while (min / div > 9)
+	while (number / div > 9)
 	{
 		div *= 10;
 	}
 	while (div != 0)
 	{
-		if (min / div < 10)
-			_putchar((min / div) + '0');
+		if (number / div < 10)
+			_putchar((number / div) + '0');
 		else
-			_putchar(((min / div) % 10) + '0');
+			_putchar(((number / div) % 10) + '0');
 		div /= 10;
-		len ++;
+		len++;
 	}
 	return (len);
 }
